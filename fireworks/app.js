6ed45cd0e34ccb0816e8b15e2af3c4a2d60e3a33
kimjaehu <kimjaehu@gmail.com>
2021-04-25 00:33:34 -0400

    add files

--- fireworks/app.js
+++ fireworks/app.js
@@ -19,6 +19,39 @@ const FIREWORK_COLORS = [
 
 class App {
   constructor() {
+    this.backBtn = document.createElement("div");
+    this.backBtn.classList.add("back-btn");
+
+    this.backA = document.createElement("a");
+    this.backA.classList.add("back-btn__link");
+    this.backA.setAttribute("href", "/");
+    this.backSvg = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "svg"
+    );
+    this.backSvg.classList.add("back-btn__svg");
+    this.backSvg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+    this.backSvg.setAttributeNS(null, "viewBox", "0 0 24 24");
+    this.backPath = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "path"
+    );
+    this.backPath.setAttributeNS(
+      null,
+      "d",
+      "M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"
+    );
+
+    this.backSpan = document.createElement("span");
+    this.backSpan.classList.add("back-btn__text");
+    this.backSpan.innerHTML = "Back to noon"
+
+    this.backSvg.appendChild(this.backPath);
+    this.backA.append(this.backSvg, this.backSpan);
+    this.backBtn.appendChild(this.backA);
+    
+    document.body.appendChild(this.backBtn);
+
     this.mouseCursor = document.createElement("div");
     this.mouseCursor.className = "cursor";
     document.body.appendChild(this.mouseCursor);
